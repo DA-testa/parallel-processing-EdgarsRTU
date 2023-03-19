37b61ff1f89295d7eb3e6a5ed96d6fb336da6993
EdgarsRTU <125836757+EdgarsRTU@users.noreply.github.com>
2023-03-19 23:05:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-# python3
-
+import heapq
+ 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    W = [(0, i) for i in range(n)]
+    heapq.heapify(W)
+    for i in range(m):
+        laiks = data[i]
+        a_laiks, W = heapq.heappop(W)
+        if output:
+            b_laiks = max(a_laiks, output[-1][1])
+        else:
+            b_laiks = a_laiks
+        a_time = b_laiks + laiks
+        output.append((W, b_laiks))
+        heapq.heappush(W, (a_laiks, W))
     return output
-
+ 
+ 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
-
-
-
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+ 
+    result = parallel_processing(n, m, data)
+ 
+    for W, b_laiks in result:
+        print(W, b_laiks)
+ 
+ 
 if __name__ == "__main__":
     main()
